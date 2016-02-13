629cb9b0da70203617c65f9b9ff14fa2dbfe54bd
Open Pinball Project <openpinballproject@gmail.com>
2016-02-13 17:15:08 +0000

    Gen2 Firmware: Added Gen2.rev0.1.0.0.cyacd as first official OPP Gen2 firmware image.
    Kicad:
    Finished surface mount Gen2 layout.  Created panelized PCB for ITeadStudio order.

---
+++ Kicad/1017-IncandSMG2/build.bat
@@ -0,0 +1,3 @@
+cd Gerber
+"c:\Program Files\7-Zip\7z.exe" a -tzip Gerber.zip *.drl *.g??
+cd ..
---
+++ Kicad/Panel-20160212/build.bat
@@ -0,0 +1,4 @@
+cd Gerber
+rename *.gbr *.gml
+"c:\Program Files\7-Zip\7z.exe" a -tzip Gerber.zip *.drl *.g??
+cd ..
