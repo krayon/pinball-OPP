f070eafc8953d42fdc8307fa65f8dce4afed8fe7
Open Pinball Project <openpinballproject@gmail.com>
2017-05-04 10:50:47 +0000

    Updated brdIntf.odt/brdIntf.pdf to correct pinout documentation for solenoid wing board.

---
+++ Python/Gen2Test/garyCfg.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#
+#===============================================================================
+#
+#                         OOOO
+#                       OOOOOOOO
+#      PPPPPPPPPPPPP   OOO    OOO   PPPPPPPPPPPPP
+#    PPPPPPPPPPPPPP   OOO      OOO   PPPPPPPPPPPPPP
+#   PPP         PPP   OOO      OOO   PPP         PPP
+#  PPP          PPP   OOO      OOO   PPP          PPP
+#  PPP          PPP   OOO      OOO   PPP          PPP
+#  PPP          PPP   OOO      OOO   PPP          PPP
+#   PPP         PPP   OOO      OOO   PPP         PPP
+#    PPPPPPPPPPPPPP   OOO      OOO   PPPPPPPPPPPPPP
+#     PPPPPPPPPPPPP   OOO      OOO   PPP
+#               PPP   OOO      OOO   PPP
+#               PPP   OOO      OOO   PPP
+#               PPP   OOO      OOO   PPP
+#               PPP    OOO    OOO    PPP
+#               PPP     OOOOOOOO     PPP
+#              PPPPP      OOOO      PPPPP
+#
+# @file:   testerCfg.py
+# @author: Hugh Spahr
+# @date:   05/08/2016
+#
+# @note:   Open Pinball Project
+#          Copyright 2012-2015, Hugh Spahr
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#===============================================================================
+#
+# This configures a processor board to be used as a wing board tester.  Wing 0
+# is configured as an incandescent board, and wing 1 is a solenoid board tester.
+#
+#===============================================================================
+
+testVers = '00.00.01'
+
+import rs232Intf
+
+# Config inputs as all state inputs
+wingCfg = [ [ rs232Intf.WING_SOL, rs232Intf.WING_INCAND, rs232Intf.WING_INP, rs232Intf.WING_INP ] ]
+
+# Config inputs as all state inputs
+inpCfg = [ [ rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, \
+             rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE, rs232Intf.CFG_INP_STATE ] ]
+
+# solenoid config
+solCfg  = [ [ rs232Intf.CFG_SOL_USE_SWITCH, '\x10', '\x04', rs232Intf.CFG_SOL_USE_SWITCH, '\x10', '\x04', \
+                rs232Intf.CFG_SOL_USE_SWITCH, '\x10', '\x00', rs232Intf.CFG_SOL_USE_SWITCH, '\x10', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00', \
+                '\x00', '\x00', '\x00', '\x00', '\x00', '\x00' ] ]
+
