25580f33f7e8692b03337837a17e2a60b1c61e1b
Open Pinball Project <openpinballproject@gmail.com>
2018-04-25 01:19:53 +0000

    Kicad:  Rename Panel-1018/Gerber/Gerber.zip to Panel-1018.zip.  Fix title on PCB legend.  Create Panel-1019 and Panel-1020 to panelize solenoid and incandescent planks.

---
+++ Kicad/Panel-1019/build.bat
@@ -0,0 +1,5 @@
+cd Gerber
+del *.gml
+rename *.gbr *.gml
+"c:\Program Files\7-Zip\7z.exe" a -tzip Panel-1019.zip *.drl *.g??
+cd ..
---
+++ Kicad/Panel-1020/build.bat
@@ -0,0 +1,5 @@
+cd Gerber
+del *.gml
+rename *.gbr *.gml
+"c:\Program Files\7-Zip\7z.exe" a -tzip Panel-1020.zip *.drl *.g??
+cd ..
